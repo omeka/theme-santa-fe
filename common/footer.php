1203c3ad353d6ca142041bae5e073be7fa7e5ee4
Jeremy Boggs <jeremy@clioweb.org>
2011-04-01 19:23:00 +0000

    Updates footer markup to more closely match defaults.
    Removes right justification of some footer elements, and updates footer text color.

--- common/footer.php
+++ common/footer.php
@@ -1,16 +1,19 @@
 <div id="footer">
     
-	<?php echo get_theme_option('Footer Text'); ?>
-    <?php if ((get_theme_option('Display Footer Copyright') !== '0') && $copyright = settings('copyright')): ?>
-        <p><?php echo $copyright; ?></p>
-    <?php endif; ?>
-	<ul class="navigation">	    
-	    <?php echo public_nav_main(array('Home' => uri(''), 'Browse Items' => uri('items'), 'Browse Collections'=>uri('collections')));
-	?>
-	</ul>
-	<p>Powered by <a href="http://omeka.org">Omeka</a>.</p>
-	
-	<?php echo plugin_footer(); ?>
+    <ul class="navigation">
+		<?php echo public_nav_main(array('Home' => uri(''), 'Browse Items' => uri('items'), 'Browse Collections'=>uri('collections')));
+    	?>
+    </ul>
+
+    <div id="footer-text">
+        <?php echo get_theme_option('Footer Text'); ?>
+        <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = settings('copyright')): ?>
+            <p><?php echo $copyright; ?></p>
+        <?php endif; ?>
+        <p>Proudly powered by <a href="http://omeka.org">Omeka</a>.</p>
+    </div>
+
+	<?php plugin_footer(); ?>
 	
 </div><!-- end footer -->
 
