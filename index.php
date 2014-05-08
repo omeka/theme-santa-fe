00d36c1c34ee55c17c785e0481a57004d918eef6
Kim Nguyen <design@kimisgold.com>
2014-05-08 15:07:06 -0400

    Prioritize featured records over recent items on homepage in markup, standardize item styles.

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
-<?php echo head(array('bodyid'=>'home')); ?>
+<?php echo head(array('bodyclass'=>'home')); ?>
 
 <?php if (get_theme_option('Homepage Text')): ?>
     <aside id="intro" role="introduction">
 <p><?php echo get_theme_option('Homepage Text'); ?></p>
 </aside>
-<?php endif; ?>
-
-<?php
-$recentItems = get_theme_option('Homepage Recent Items');
-if ($recentItems === null || $recentItems === ''):
-    $recentItems = 3;
-else:
-    $recentItems = (int) $recentItems;
-endif;
-if ($recentItems):
-?>
-<div id="recent-items">
-    <h2><?php echo __('Recently Added Items'); ?></h2>
-    <?php echo recent_items($recentItems); ?>
-    <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
-</div><!--end recent-items -->
 <?php endif; ?>
 
 <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
@@ -43,6 +27,22 @@ if ($recentItems):
         && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
 <!-- Featured Exhibit -->
 <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+<?php endif; ?>
+
+<?php
+$recentItems = get_theme_option('Homepage Recent Items');
+if ($recentItems === null || $recentItems === ''):
+    $recentItems = 3;
+else:
+    $recentItems = (int) $recentItems;
+endif;
+if ($recentItems):
+?>
+<div id="recent-items">
+    <h2><?php echo __('Recently Added Items'); ?></h2>
+    <?php echo recent_items($recentItems); ?>
+    <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
+</div><!--end recent-items -->
 <?php endif; ?>
 
 <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
